c7da252ccf00a984301fb676537f2ff614f85645
juhidesai <jddesai@ncsu.edu>
2015-09-01 19:40:50 +0000

    screenshots for think3

--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -2,7 +2,7 @@
 ### so the function call appears before the definitions. 
 ### Run the program and see what error message you get.
 
-"""
+
 repeat_lyrics()
 
 def print_lyrics():
@@ -13,13 +13,13 @@ def repeat_lyrics():
     print_lyrics()
     print_lyrics()
      
-"""
+
 
 ### Exercise 2: Move the function call back to the bottom 
 ### and move the definition of print_lyrics after the 
 ### definition of repeat_lyrics. What happens when you run this program?
 
-"""
+
 def repeat_lyrics():
     print_lyrics()
     print_lyrics()
@@ -29,7 +29,7 @@ def print_lyrics():
     print "I sleep all night and I work all day."
      
 repeat_lyrics()
-"""
+
 
 
 ### Exercise 3: Python provides a built-in function called len that returns the length of a string, 
@@ -41,14 +41,14 @@ repeat_lyrics()
 
 ### >>> right_justify('allen')
 
-"""
+
 def right_justify(input_str):
     str_len = len(input_str)
     leading_spaces = 70 - str_len
     print " "*leading_spaces + input_str
     
-right_justify("Rohit Arora")
-"""
+right_justify("This is a string")
+
 
 ### Exercise 4: A function object is a value you can assign to a variable or pass as an argument. 
 ### For example, do_twice is a function that takes a function object as an argument and calls it twice:
@@ -73,7 +73,6 @@ right_justify("Rohit Arora")
 ###    5. Define a new function called do_four that takes a function object and a value and calls the function four times, 
 ###    passing the value as a parameter. There should be only two statements in the body of this function, not four.
 
-""" 
 def do_twice(f,v):
     f(v)
     f(v)
@@ -90,7 +89,7 @@ do_twice(print_twice, 'spam')
 
 do_four(print_twice, 'spam_new')
 
-"""
+
 
 ### Exercise 5: This exercise can be done using only the statements and other features we have learned so far.
 
